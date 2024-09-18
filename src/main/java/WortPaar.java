984d848376d895e2137b4c82ce759e80f77586b2
amadeus <aabdulkarim@student.tgm.ac.at>
2024-09-18 17:37:44 +0200

    Angefangen mit der Implementierung von weiteren Klassen

---
+++ src/main/java/WortPaar.java
@@ -0,0 +1,13 @@
+/**
+ * Das Rückgrat der gesamten Applikation. Besteht aus einem Wort zugehörig zu der Bild URL
+ *
+ * @author Amadeus Abdulkarim
+ * @version 18-09-2024
+ */
+public class WortPaar {
+    private String wort;
+    private String bildURL;
+
+
+
+}
