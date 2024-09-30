b2fe8102cd5d4ab636241e89e04f7adb942d4e38
Abdulkarim <aabdulkarim@student.tgm.ac.at>
2024-09-30 23:56:44 +0200

    Ansattz Implementieren vom GUI

---
+++ src/main/java/control/ControlWortTrainer.java
@@ -0,0 +1,13 @@
+package control;
+
+import model.WortTrainer;
+import view.ViewWortTrainer;
+
+public class ControlWortTrainer{
+
+    private WortTrainer model;
+    private ViewWortTrainer view;
+    private SpeichernLadenStrategie speicherStrategie;
+
+
+}
