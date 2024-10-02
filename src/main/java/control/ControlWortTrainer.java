9c83271b6e614d55389dbfd3de082ae8159fc279
amadeus <aabdulkarim@student.tgm.ac.at>
2024-10-02 14:50:36 +0200

    Löschen von unnötigen FlatLaf imports

--- src/main/java/control/ControlWortTrainer.java
+++ src/main/java/control/ControlWortTrainer.java
@@ -1,5 +1,6 @@
 package control;
 
+import model.WortPaar;
 import model.WortTrainer;
 import persistance.SpeichernLadenJson;
 import persistance.SpeichernLadenStrategie;
@@ -8,41 +9,95 @@ import view.ViewWortTrainer;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
+/**
+ * Die ControlWortTrainer Klasse ist der Controller der MVC Struktur
+ * Diese Komponente hält die View und das Model und bietet die Logik der Applikation
+ *
+ * @author Amadeus Abdulkarim
+ * @version 02-10-2024
+ */
 public class ControlWortTrainer implements ActionListener{
 
     private WortTrainer model;
-    private JFrame view;
+    private ViewWortTrainer view;
 
     private SpeichernLadenStrategie speicherStrategie;
 
     /**
-     * Controller
+     * Konstruktor der Controller Klasse ohne Parameter
      */
     public ControlWortTrainer(){
         this.model = new WortTrainer();
-        this.view = new ViewWortTrainer();
+        this.view = new ViewWortTrainer(this);
         this.speicherStrategie = new SpeichernLadenJson();
+        setBildUrl();
+
     }
 
     /**
+     * Konstruktor der Controller Klasse mit Parameter
      *
-     * @param model
-     * @param view
-     * @param speicherStrategie
+     * @param model vorhandenes Model
+     * @param view vorhandene View
+     * @param speicherStrategie vorhandene Speicherstrategie, welche vom Interface SpeicherStrategie implementiert
      */
     public ControlWortTrainer(WortTrainer model, ViewWortTrainer view, SpeichernLadenStrategie speicherStrategie){
         this.model = model;
         this.view = view;
         this.speicherStrategie = speicherStrategie;
+        setBildUrl();
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e){
+        String ac = e.getActionCommand();
+
+        // Antwort Auf Save Button
+        if (ac.equalsIgnoreCase("save")) {
+            try {
+                speicherStrategie.speichern(model);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+
+        // Antwort auf Load Button
+        else if (ac.equalsIgnoreCase("load")) {
+            try {
+                WortTrainer geladenesModel = speicherStrategie.laden();
+                this.model = geladenesModel;
+                this.view.setStatistik(this.model.getStatistikManager());
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+        // Antwort auf Nextes Wort
+        else if (ac.equalsIgnoreCase("next")) {
+            String userInput = view.getUserInput();
+            this.model.checkAntwort(userInput);
+            this.view.setStatistik(model.getStatistikManager());
+
+            setBildUrl();
+
+        }
+
 
     }
 
-    public void setSpeicherPfad(String pfad){
+    public void setBildUrl() {
+        WortPaar aktuellesWortPaar = this.model.getAktuellesWort();
+
+        try {
+            this.view.setBildUrl(aktuellesWortPaar.getBildURL());
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 }
--- src/main/java/view/ViewWortTrainer.java
+++ src/main/java/view/ViewWortTrainer.java
@@ -1,33 +1,35 @@
 package view;
 
+import control.ControlWortTrainer;
 import model.StatistikManager;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class ViewWortTrainer extends JFrame{
+public class ViewWortTrainer extends JFrame {
 
     private JLabel richtig = new JLabel("+");
     private JLabel falsch = new JLabel("-");
-    private JLabel insgesamt = new JLabel("");
+    private JLabel insgesamt = new JLabel("||");
 
     private ImageIcon imageIcon = new ImageIcon();
+    private JLabel imageLabel = new JLabel(imageIcon);
 
     private JButton nextButton = new JButton("Naechstes Wort");
 
     private JButton speicherButton = new JButton("Speichern");
     private JButton ladeButton = new JButton("Laden");
 
+    private JTextField textField = new JTextField();
 
-    public ViewWortTrainer() {
+    public ViewWortTrainer(ControlWortTrainer control) {
         setTitle("Wort Trainer");
-        setSize(300, 200);
+        setSize(1000, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        BorderLayout borderLayout = new BorderLayout();
-        setLayout(borderLayout);
+        setLayout(new BorderLayout());
 
         // Look and Feel
         try {
@@ -35,7 +37,7 @@ public class ViewWortTrainer extends JFrame{
                 System.out.println(info.getName() + " - " + info.getClassName());
             }
 
-            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
         } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
                  IllegalAccessException e) {
             e.printStackTrace();
@@ -43,9 +45,19 @@ public class ViewWortTrainer extends JFrame{
 
 
         this.add(statistikPanel(), BorderLayout.PAGE_START);
-        this.add(midPanel(), BorderLayout.CENTER);
-        this.add(nextButton, BorderLayout.LINE_END);
+        this.add(speicherButton, BorderLayout.LINE_START);
+        this.add(imageLabel, BorderLayout.CENTER);
+        this.add(ladeButton, BorderLayout.LINE_END);
         this.add(optionsPanel(), BorderLayout.PAGE_END);
+
+        speicherButton.addActionListener(control);
+        speicherButton.setActionCommand("save");
+
+        ladeButton.addActionListener(control);
+        ladeButton.setActionCommand("load");
+
+        nextButton.addActionListener(control);
+        nextButton.setActionCommand("next");
 
 
         setVisible(true);
@@ -53,22 +65,20 @@ public class ViewWortTrainer extends JFrame{
     }
 
 
-
-
     /**
      * Das ist ein JPanel für die Statistikanzeige
      *
      * @return JPanel
      */
-    public JPanel statistikPanel(){
+    public JPanel statistikPanel() {
         JPanel statistikPanel = new JPanel();
 
         richtig.setText("+");
-        insgesamt.setText("");
+        insgesamt.setText("||");
         falsch.setText("-");
 
-//        richtig.setBackground(Color.green);
-//        falsch.setBackground(Color.red);
+        richtig.setBackground(Color.green);
+        falsch.setBackground(Color.red);
 
         statistikPanel.add(richtig);
         statistikPanel.add(insgesamt);
@@ -78,36 +88,18 @@ public class ViewWortTrainer extends JFrame{
     }
 
 
-    /**
-     * Das ist ein JPanel für die mittlere Anzeige, dabei sieht man das Image basierend auf einer BildURL
-     * und ein Button für das nächste WortPaar
-     *
-     * @return JPanel
-     */
-    public JPanel midPanel(){
-        JPanel midPanel = new JPanel();
 
-        try {
-            JLabel imageLabel = new JLabel(imageIcon);
-
-            midPanel.add(imageLabel);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return midPanel;
-    }
 
 
     /**
-     * Das ist ein JPanel für die Optionen bzw. Einstellungen. Man kann Speichern und Laden.
      *
      * @return JPanel
      */
-    public JPanel optionsPanel(){
+    public JPanel optionsPanel() {
         JPanel optionsPanel = new JPanel();
-        optionsPanel.add(this.speicherButton);
-        optionsPanel.add(this.ladeButton);
+        this.textField.setPreferredSize(new Dimension(330, 75));
+        optionsPanel.add(this.textField);
+        optionsPanel.add(this.nextButton);
 
         return optionsPanel;
     }
@@ -118,37 +110,41 @@ public class ViewWortTrainer extends JFrame{
      *
      * @param sm StatistikManager enthält alle Daten
      */
-    public void setStatistik(StatistikManager sm){
+    public void setStatistik(StatistikManager sm) {
         this.richtig.setText("+" + sm.getAnzahlRichtig());
         this.falsch.setText("-" + sm.getAnzahlFalsch());
-        this.insgesamt.setText("" + sm.getInsgesamt());
+        this.insgesamt.setText("|" + sm.getInsgesamt() + "|");
     }
 
     /**
      * Setzt die URL vom Bild
+     *
      * @param url String
      * @throws MalformedURLException
      */
-    public void setBildUrl(String url) throws MalformedURLException{
+    public void setBildUrl(String url) throws MalformedURLException {
         // Falls das nicht funktioniert muss das Image Label neu gesetzt werden
         this.imageIcon = new ImageIcon(new URL(url));
+        this.imageLabel.setIcon(imageIcon);
     }
 
     /**
      * Setzt jede Komponenten neu auf.
      */
-    public void reset(){
+    public void reset() {
         richtig = new JLabel("+");
         falsch = new JLabel("-");
-        insgesamt = new JLabel();
+        insgesamt = new JLabel("||");
 
         imageIcon = new ImageIcon();
+        imageLabel = new JLabel();
 
         speicherButton = new JButton("Speichern");
         ladeButton = new JButton("Laden");
     }
 
 
-
-
+    public String getUserInput() {
+        return this.textField.getText();
+    }
 }
